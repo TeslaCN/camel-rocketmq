e2aa6a83e99c609e4d9093f33e8d2f5d1755f0f9
吴伟杰 <wuweijie@apache.org>
2020-12-24 08:47:50 +0800

    Move packages to icu.wwj

---
+++ src/main/java/icu/wwj/camel/component/rocketmq/RocketMQComponent.java
@@ -0,0 +1,166 @@
+/*
+ *    Copyright 2020  Wu Weijie
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package icu.wwj.camel.component.rocketmq;
+
+import org.apache.camel.spi.Metadata;
+import org.apache.camel.support.DefaultComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * @author wuweijie
+ */
+public class RocketMQComponent extends DefaultComponent {
+
+    private final Logger logger = LoggerFactory.getLogger(RocketMQComponent.class);
+
+    @Metadata(required = true)
+    private String topicName;
+
+    @Metadata(label = "producer")
+    private String producerGroup;
+
+    @Metadata(label = "consumer")
+    private String consumerGroup;
+
+    @Metadata(label = "consumer", defaultValue = "*")
+    private String subscribeTags = "*";
+
+    @Metadata(label = "common", defaultValue = "")
+    private String sendTag = "";
+
+    @Metadata(label = "common", defaultValue = "localhost:9876")
+    private String namesrvAddr = "localhost:9876";
+
+    @Metadata(label = "producer")
+    private String replyToTopic;
+
+    @Metadata(label = "producer")
+    private String replyToConsumerGroup;
+
+    @Metadata(label = "advance", defaultValue = "10000")
+    private Long requestTimeout = 10000L;
+
+    @Metadata(label = "advance", defaultValue = "1000")
+    private Long requestTimeoutCheckerInterval = 1000L;
+
+    @Metadata(label = "producer", defaultValue = "false")
+    private Boolean waitForSendResult = false;
+
+    @Override
+    protected RocketMQEndpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
+
+        RocketMQEndpoint endpoint = new RocketMQEndpoint(uri, this);
+        endpoint.setTopicName(remaining);
+        setProperties(endpoint, parameters);
+        if (endpoint.getNamesrvAddr() == null) {
+            endpoint.setNamesrvAddr("localhost:9876");
+        }
+        return endpoint;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public String getSubscribeTags() {
+        return subscribeTags;
+    }
+
+    public void setSubscribeTags(String subscribeTags) {
+        this.subscribeTags = subscribeTags;
+    }
+
+    public String getSendTag() {
+        return sendTag;
+    }
+
+    public void setSendTag(String sendTag) {
+        this.sendTag = sendTag;
+    }
+
+    public String getNamesrvAddr() {
+        return namesrvAddr;
+    }
+
+    public void setNamesrvAddr(String namesrvAddr) {
+        this.namesrvAddr = namesrvAddr;
+    }
+
+    public String getProducerGroup() {
+        return producerGroup;
+    }
+
+    public void setProducerGroup(String producerGroup) {
+        this.producerGroup = producerGroup;
+    }
+
+    public String getConsumerGroup() {
+        return consumerGroup;
+    }
+
+    public void setConsumerGroup(String consumerGroup) {
+        this.consumerGroup = consumerGroup;
+    }
+
+    public String getReplyToTopic() {
+        return replyToTopic;
+    }
+
+    public void setReplyToTopic(String replyToTopic) {
+        this.replyToTopic = replyToTopic;
+    }
+
+    public String getReplyToConsumerGroup() {
+        return replyToConsumerGroup;
+    }
+
+    public void setReplyToConsumerGroup(String replyToConsumerGroup) {
+        this.replyToConsumerGroup = replyToConsumerGroup;
+    }
+
+    public Long getRequestTimeout() {
+        return requestTimeout;
+    }
+
+    public void setRequestTimeout(Long requestTimeout) {
+        this.requestTimeout = requestTimeout;
+    }
+
+    public Long getRequestTimeoutCheckerInterval() {
+        return requestTimeoutCheckerInterval;
+    }
+
+    public void setRequestTimeoutCheckerInterval(Long requestTimeoutCheckerInterval) {
+        this.requestTimeoutCheckerInterval = requestTimeoutCheckerInterval;
+    }
+
+    public Boolean getWaitForSendResult() {
+        return waitForSendResult;
+    }
+
+    public void setWaitForSendResult(Boolean waitForSendResult) {
+        this.waitForSendResult = waitForSendResult;
+    }
+}
---
+++ src/main/java/icu/wwj/camel/component/rocketmq/RocketMQMessageConverter.java
@@ -0,0 +1,90 @@
+/*
+ *    Copyright 2020  Wu Weijie
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package icu.wwj.camel.component.rocketmq;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.support.DefaultMessage;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.BODY_CRC;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.BORN_HOST;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.BORN_TIMESTAMP;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.BROKER_NAME;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.COMMIT_LOG_OFFSET;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.KEY;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.MSG_ID;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.PREPARED_TRANSACTION_OFFSET;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.QUEUE_ID;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.QUEUE_OFFSET;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.RECONSUME_TIMES;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.STORE_HOST;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.STORE_SIZE;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.STORE_TIMESTAMP;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.SYS_FLAG;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.TAG;
+import static icu.wwj.camel.component.rocketmq.RocketMQConstants.TOPIC;
+
+/**
+ * @author wuweijie
+ */
+public class RocketMQMessageConverter {
+
+    public void setExchangeHeadersByMessageExt(Exchange exchange, MessageExt messageExt) {
+        exchange.getIn().setHeader(BROKER_NAME, messageExt.getBrokerName());
+        exchange.getIn().setHeader(QUEUE_ID, messageExt.getQueueId());
+        exchange.getIn().setHeader(STORE_SIZE, messageExt.getStoreSize());
+        exchange.getIn().setHeader(QUEUE_OFFSET, messageExt.getQueueOffset());
+        exchange.getIn().setHeader(SYS_FLAG, messageExt.getSysFlag());
+        exchange.getIn().setHeader(BORN_TIMESTAMP, messageExt.getBornTimestamp());
+        exchange.getIn().setHeader(BORN_HOST, messageExt.getBornHost());
+        exchange.getIn().setHeader(STORE_TIMESTAMP, messageExt.getStoreTimestamp());
+        exchange.getIn().setHeader(STORE_HOST, messageExt.getStoreHost());
+        exchange.getIn().setHeader(MSG_ID, messageExt.getMsgId());
+        exchange.getIn().setHeader(COMMIT_LOG_OFFSET, messageExt.getCommitLogOffset());
+        exchange.getIn().setHeader(BODY_CRC, messageExt.getBodyCRC());
+        exchange.getIn().setHeader(RECONSUME_TIMES, messageExt.getReconsumeTimes());
+        exchange.getIn().setHeader(PREPARED_TRANSACTION_OFFSET, messageExt.getPreparedTransactionOffset());
+    }
+
+    public void populateRocketExchange(Exchange exchange, MessageExt messageExt, final boolean out) {
+        Message message = resolveMessageFrom(exchange, out);
+        populateRoutingInfoHeaders(message, messageExt);
+    }
+
+    private void populateRoutingInfoHeaders(final Message message, final MessageExt messageExt) {
+        if (messageExt != null) {
+            message.setHeader(TOPIC, messageExt.getTopic());
+            message.setHeader(TAG, messageExt.getTags());
+            message.setHeader(KEY, messageExt.getKeys());
+        }
+    }
+
+    private Message resolveMessageFrom(final Exchange exchange, final boolean out) {
+        Message message;
+        if (out) {
+            message = exchange.getOut();
+        } else {
+            if ((message = exchange.getIn()) == null) {
+                message = new DefaultMessage(exchange.getContext());
+                exchange.setIn(message);
+            }
+        }
+        return message;
+    }
+}
