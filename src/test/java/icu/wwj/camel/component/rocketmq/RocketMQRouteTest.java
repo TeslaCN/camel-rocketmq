0d00dd8b3235e062566e191af58c5b1749b2d87c
吴伟杰 <wuweijie@apache.org>
2022-11-29 22:31:00 +0800

    Fix inout missing body and complete tests (#6)
    * Add more tests
    
    * Fix in-out pattern

--- src/test/java/icu/wwj/camel/component/rocketmq/RocketMQRouteTest.java
+++ src/test/java/icu/wwj/camel/component/rocketmq/RocketMQRouteTest.java
@@ -5,24 +5,36 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.apache.rocketmq.broker.BrokerController;
+import org.apache.rocketmq.namesrv.NamesrvController;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RocketMQRouteTest extends CamelTestSupport {
 
-    private static final String START_ENDPOINT_URI = "rocketmq:START_TOPIC?namesrvAddr=127.0.0.1:59876&producerGroup=p1&consumerGroup=c1";
-    
+    private static final int NAMESRV_PORT = 59876;
+
+    private static final String NAMESRV_ADDR = "127.0.0.1:" + NAMESRV_PORT;
+
+    private static final String START_ENDPOINT_URI = "rocketmq:START_TOPIC?producerGroup=p1&consumerGroup=c1";
+
     private static final String RESULT_ENDPOINT_URI = "mock:result";
+
+    public static final String EXPECTED_MESSAGE = "hello, RocketMQ.";
+
+    private static NamesrvController namesrvController;
+
+    private static BrokerController brokerController;
 
     private MockEndpoint resultEndpoint;
 
     @BeforeAll
     static void beforeAll() throws Exception {
-        EmbeddedRocketMQServer.createAndStartNamesrv(59876);
-        EmbeddedRocketMQServer.createAndStartBroker("127.0.0.1:59876");
-        EmbeddedRocketMQServer.createTopic("127.0.0.1:59876", "DefaultCluster","START_TOPIC");
-        EmbeddedRocketMQServer.createTopic("127.0.0.1:59876", "DefaultCluster","IMTERMEDIATE_TOPIC");
+        namesrvController = EmbeddedRocketMQServer.createAndStartNamesrv(NAMESRV_PORT);
+        brokerController = EmbeddedRocketMQServer.createAndStartBroker(NAMESRV_ADDR);
+        EmbeddedRocketMQServer.createTopic(NAMESRV_ADDR, "DefaultCluster", "START_TOPIC");
     }
 
     @Override
@@ -35,7 +47,9 @@ public class RocketMQRouteTest extends CamelTestSupport {
     @Override
     protected CamelContext createCamelContext() throws Exception {
         CamelContext camelContext = super.createCamelContext();
-        camelContext.addComponent("rocketmq", new RocketMQComponent());
+        RocketMQComponent rocketMQComponent = new RocketMQComponent();
+        rocketMQComponent.setNamesrvAddr(NAMESRV_ADDR);
+        camelContext.addComponent("rocketmq", rocketMQComponent);
         return camelContext;
     }
 
@@ -52,11 +66,17 @@ public class RocketMQRouteTest extends CamelTestSupport {
 
     @Test
     public void testRouteWithTextMessage() throws Exception {
-        resultEndpoint.expectedBodiesReceived("hello, RocketMQ.");
+        resultEndpoint.expectedBodiesReceived(EXPECTED_MESSAGE);
         resultEndpoint.message(0).predicate(exchange -> !exchange.getIn().getHeader(RocketMQConstants.MSG_ID, String.class).isBlank());
-    
-        template.sendBody(START_ENDPOINT_URI, "hello, RocketMQ.");
+
+        template.sendBody(START_ENDPOINT_URI, EXPECTED_MESSAGE);
 
         resultEndpoint.assertIsSatisfied();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        brokerController.shutdown();
+        namesrvController.shutdown();
     }
 }
